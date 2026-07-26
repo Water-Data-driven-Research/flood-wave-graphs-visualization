2ff626d168a4bc86591630c7dbb9b12f75d66239
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-26 14:34:33 +0200

    Add imports and empty constructor to graph_plotter.py

---
+++ src/plot_and_table_creation/graph_plotter.py
@@ -0,0 +1,14 @@
+from src.data_preparation.interfaces.graph_data_interface import (
+    GraphDataInterface
+)
+
+import networkx as nx
+import plotly
+
+
+class GraphPlotter:
+    """
+    This class creates the plot of the received flood wave graph or flood map.
+    """
+    def __init__(self, graph_data_interface: GraphDataInterface):
+        pass
