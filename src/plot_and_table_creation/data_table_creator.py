a93308bfe5d8440ecd90939e6dc3e9be458cea3f
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-26 15:55:04 +0200

    Add imports and empty constructor to data_table_creator.py

---
+++ src/plot_and_table_creation/data_table_creator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+class DataTableCreator:
+    """
+    This class creates and saves a table containing time series data about
+    station pairs.
+    """
+    def __init__(self, df: pd.DataFrame):
+        pass
