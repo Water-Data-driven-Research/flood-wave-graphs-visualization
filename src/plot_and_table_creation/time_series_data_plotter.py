ae516575d01be673eb49cde29bebb828c9866b10
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-26 14:34:54 +0200

    Add imports and empty constructor to time_series_data_plotter.py

---
+++ src/plot_and_table_creation/time_series_data_plotter.py
@@ -0,0 +1,13 @@
+from src.data_preparation.interfaces.time_series_data_interface import (
+    TimeSeriesDataInterface
+)
+
+import plotly
+
+
+class TimeSeriesDataPlotter:
+    """
+    This class creates the plot of the received time series data.
+    """
+    def __init__(self, time_series_data_interface: TimeSeriesDataInterface):
+        pass
