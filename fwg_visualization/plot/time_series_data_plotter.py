ab034651b1cf046ad87758c316d1c7052f33100a
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-27 13:01:02 +0200

    Correct import order and update documentation in time_series_data_plotter.py

--- fwg_visualization/plot/time_series_data_plotter.py
+++ fwg_visualization/plot/time_series_data_plotter.py
@@ -1,13 +1,15 @@
+import plotly
+
 from fwg_visualization.data.interfaces.time_series_data_interface import (
     TimeSeriesDataInterface
 )
-
-import plotly
 
 
 class TimeSeriesDataPlotter:
     """
     This class creates the plot of the received time series data.
+    :param TimeSeriesDataInterface time_series_data_interface: contains data to
+    be plotted
     """
     def __init__(self, time_series_data_interface: TimeSeriesDataInterface):
         pass
