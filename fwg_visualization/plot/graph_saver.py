c298ec9c7a710f86d7a82cea99aad43159f1ef93
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-27 16:00:11 +0200

    Refactored (renamed) the GraphManager to GraphSaver (no longer needs loading functionality)

---
+++ fwg_visualization/plot/graph_saver.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+class GraphSaver:
+    """
+    This static class saves a created plot as a static image.
+    """
+    @staticmethod
+    def save_graph():
+        pass
