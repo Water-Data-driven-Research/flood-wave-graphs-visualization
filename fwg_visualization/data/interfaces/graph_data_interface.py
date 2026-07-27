52e53f7143074133039faa732c55a03d28a6c541
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-27 12:26:55 +0200

    Turn data interfaces into data classes

--- fwg_visualization/data/interfaces/graph_data_interface.py
+++ fwg_visualization/data/interfaces/graph_data_interface.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 from datetime import datetime
 
 
+@dataclass
 class GraphDataInterface:
     """
     Class for storing data preprocessed in a GraphDataHandler class.
--- fwg_visualization/data/interfaces/time_series_data_interface.py
+++ fwg_visualization/data/interfaces/time_series_data_interface.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 import pandas as pd
 
 
+@dataclass
 class TimeSeriesDataInterface:
     """
     Class for storing data preprocessed in a TimeSeriesDataHandler class.
