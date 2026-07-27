db1ef1cf1814aeeb348df49d4df556f490621196
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-27 12:59:59 +0200

    Update documentation in graph_data_handler.py

--- fwg_visualization/data/graph_data_handler.py
+++ fwg_visualization/data/graph_data_handler.py
@@ -7,6 +7,7 @@ import pandas as pd
 class GraphDataHandler:
     """
     This class preprocesses a flood wave graph or flood map for visualization.
+    :param dict data: the data to be preprocessed
     """
     def __init__(self, data: dict):
         pass
