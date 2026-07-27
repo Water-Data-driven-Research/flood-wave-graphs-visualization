048b28035963c7d81abde328b175ad6d6cfd70de
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-27 12:59:47 +0200

    Update documentation in data_table_creator.py

--- fwg_visualization/table/data_table_creator.py
+++ fwg_visualization/table/data_table_creator.py
@@ -5,6 +5,7 @@ class DataTableCreator:
     """
     This class creates and saves a table containing time series data about
     station pairs.
+    :param pd.DataFrame df: contains the data to create the table from
     """
     def __init__(self, df: pd.DataFrame):
         pass
