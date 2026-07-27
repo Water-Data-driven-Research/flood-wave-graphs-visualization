d183bb35b32f9de11d2d01f8ab3062adbda6b7de
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-27 12:27:39 +0200

    Remove overwriting constructors from data classes

--- fwg_visualization/data/interfaces/graph_data_interface.py
+++ fwg_visualization/data/interfaces/graph_data_interface.py
@@ -7,5 +7,4 @@ class GraphDataInterface:
     """
     Class for storing data preprocessed in a GraphDataHandler class.
     """
-    def __init__(self, data: dict = None):
-        pass
+
--- fwg_visualization/data/interfaces/time_series_data_interface.py
+++ fwg_visualization/data/interfaces/time_series_data_interface.py
@@ -7,5 +7,4 @@ class TimeSeriesDataInterface:
     """
     Class for storing data preprocessed in a TimeSeriesDataHandler class.
     """
-    def __init__(self, statistics: pd.DataFrame = None):
-        pass
+
