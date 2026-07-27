8bc98c2aafcd227b783f7626fb14defd9ea16a25
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-27 13:00:40 +0200

    Update documentation in time_series_data_handler.py

--- fwg_visualization/data/time_series_data_handler.py
+++ fwg_visualization/data/time_series_data_handler.py
@@ -5,6 +5,7 @@ class TimeSeriesDataHandler:
     """
     This class preprocesses the received yearly and quarterly statistics for
     visualization.
+    :param dict data: the data to be preprocessed
     """
     def __init__(self, data: dict):
         pass
