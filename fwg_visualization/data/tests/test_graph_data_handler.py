39d95b00f035a5cd2b30ea6bf4b24a0f8405222a
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-31 20:42:57 +0200

    Create mock graph in test_graph_data_handler.py

--- fwg_visualization/data/tests/test_graph_data_handler.py
+++ fwg_visualization/data/tests/test_graph_data_handler.py
@@ -8,3 +8,23 @@ from fwg_visualization.data.interfaces.graph_data_interface import (
     GraphDataInterface
 )
 
+
+mock_graph = nx.DiGraph()
+
+mock_graph.add_nodes_from(
+    nodes_for_adding=[
+        ('1.0', '2000-01-01'), ('1.0', '2000-01-06'),
+        ('1.0', '2000-01-08'), ('2.0', '1999-12-31'),
+        ('2.0', '2000-01-04'), ('3.0', '2000-01-03'),
+        ('3.0', '1999-12-20'), ('5.0', '1999-12-24')
+    ]
+)
+
+mock_graph.add_edges_from(
+    ebunch_to_add=[
+        (('2.0', '1999-12-31'), ('1.0', '2000-01-01'), {'slope': 1.0}),
+        (('2.0', '2000-01-04'), ('1.0', '2000-01-06'), {'slope': 1.0}),
+        (('3.0', '2000-01-03'), ('2.0', '2000-01-04'), {'slope': 1.0})
+    ]
+)
+
