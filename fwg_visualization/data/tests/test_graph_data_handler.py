af352fffe1c674e324fa4963bf9f8aca0c85d3a9
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-07-31 20:42:16 +0200

    Add imports to test_graph_data_handler.py

---
+++ fwg_visualization/data/tests/test_graph_data_handler.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+import networkx as nx
+import pytest
+
+from fwg_visualization.data.graph_data_handler import GraphDataHandler
+from fwg_visualization.data.interfaces.graph_data_interface import (
+    GraphDataInterface
+)
+
