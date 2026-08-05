ab022f4b7db554784c109e0bdd24ff19cabbd4bb
balazsmindszenti <mindszenti.balazs@gmail.com>
2026-08-05 13:34:01 +0200

    dev/implement-graph-data-handling (#11)
    * Change imports, fill constructor and add empty run method
    
    * Remove pandas import, fill run method and create structure
    
    * Implement get_min_date method
    
    * Implement get_stations method
    
    * Implement get_positions method
    
    * Change GraphDataInterface member variables
    
    * Change what parameter GraphDataHandler receives
    
    * Update documentation in graph_data_handler.py
    
    * Update documentation in graph_data_handler.py
    
    * Fix get_stations method
    
    * Add empty files for graph data handling testing
    
    * Add imports to test_graph_data_handler.py
    
    * Create mock graph in test_graph_data_handler.py
    
    * Add graph_data_interface fixture
    
    * Add min_date test
    
    * Add stations test
    
    * Add position test
    
    * Implement Dockerfile_graph_data_handler_test
    
    * Implement graph_data_handling_test.yml
    
    * Update documentation in graph_data_handler.py
    
    * Fix py file name in Dockerfile_graph_data_handler_test
    
    * Place mock_graph inside a pytest fixture
    
    * Update graph_data_interface fixture documentation
    
    * Add documentation for test functions
    
    * Add line break
    
    * Remove calling run function in constructor
    
    * Remove line breaks in enumerate call
    
    * Add type annotation to member variables of GraphDataHandler
    
    * Change get_min_date structure
    
    * Change get_stations structure
    
    * Change get_positions structure
    
    * Change run function documentation
    
    * Add run function to test_graph_data_handler.py to fix the tests

--- fwg_visualization/data/graph_data_handler.py
+++ fwg_visualization/data/graph_data_handler.py
@@ -1,16 +1,75 @@
 from datetime import datetime
 
 import networkx as nx
-import pandas as pd
+
+from fwg_visualization.data.interfaces.graph_data_interface import (
+    GraphDataInterface
+)
 
 
 class GraphDataHandler:
     """
     This class preprocesses a flood wave graph or flood map for visualization.
     """
-    def __init__(self, data: dict):
+    def __init__(self, graph: nx.DiGraph):
         """
         Constructor.
-        :param dict data: the data to be preprocessed (the fwg or flood map)
+        :param nx.DiGraph graph: the fwg or flood map to be preprocessed
         """
-        pass
+        self.graph_nodes = graph.nodes()
+
+        self.graph_data_interface = GraphDataInterface()
+        self.min_date: datetime = datetime.min
+        self.stations: list = []
+        self.pos: dict = {}
+
+    def run(self):
+        """
+        Run function, extracts the required data (min_date, stations, pos),
+        then instantiates a GraphDataInterface to store this data.
+        """
+        self.get_min_date()
+        self.get_stations()
+        self.get_positions()
+
+        self.graph_data_interface = GraphDataInterface(
+            min_date=self.min_date,
+            stations=self.stations,
+            pos=self.pos
+        )
+
+    def get_min_date(self):
+        """
+        Fills the self.min_date datetime by finding the earliest date among
+        the dates of the nodes of the flood wave graph or flood map.
+        """
+        min_date_temp = min(
+            [node[1] for node in self.graph_nodes]
+        )
+        self.min_date = datetime.strptime(min_date_temp, '%Y-%m-%d')
+
+    def get_stations(self):
+        """
+        Fills the self.stations list by acquiring and sorting a list of the
+        stations in the flood wave graph or flood map.
+        """
+        self.stations = sorted(list(set(
+            [float(node[0]) for node in self.graph_nodes]
+        )))
+
+    def get_positions(self):
+        """
+        Fills the self.pos dictionary, which maps the nodes to their eventual
+        positions on the grid of the plot.
+        """
+        station_to_idx = {
+            station: i for i, station in enumerate(self.stations)
+        }
+
+        for node in self.graph_nodes:
+            node_date = datetime.strptime(node[1], '%Y-%m-%d')
+
+            x_coord = (node_date - self.min_date).days
+            y_coord = station_to_idx[float(node[0])]
+
+            self.pos[node] = (x_coord, y_coord)
---
+++ fwg_visualization/data/tests/test_graph_data_handler.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+
+import networkx as nx
+import pytest
+
+from fwg_visualization.data.graph_data_handler import GraphDataHandler
+from fwg_visualization.data.interfaces.graph_data_interface import (
+    GraphDataInterface
+)
+
+
+@pytest.fixture()
+def mock_graph() -> nx.DiGraph:
+    """
+    Creates a mock graph on which the codebase can be tested.
+    :return nx.DiGraph: the mock graph on which we can run tests
+    """
+    mock_graph = nx.DiGraph()
+    mock_graph.add_nodes_from(
+        nodes_for_adding=[
+            ('1.0', '2000-01-01'), ('1.0', '2000-01-06'),
+            ('1.0', '2000-01-08'), ('2.0', '1999-12-31'),
+            ('2.0', '2000-01-04'), ('3.0', '2000-01-03'),
+            ('3.0', '1999-12-20'), ('5.0', '1999-12-24')
+        ]
+    )
+    mock_graph.add_edges_from(
+        ebunch_to_add=[
+            (('2.0', '1999-12-31'), ('1.0', '2000-01-01'), {'slope': 1.0}),
+            (('2.0', '2000-01-04'), ('1.0', '2000-01-06'), {'slope': 1.0}),
+            (('3.0', '2000-01-03'), ('2.0', '2000-01-04'), {'slope': 1.0})
+        ]
+    )
+    return mock_graph
+
+
+@pytest.fixture
+def graph_data_interface(mock_graph: nx.DiGraph) -> GraphDataInterface:
+    """
+    Extracts the necessary data from the mock graph with a GraphDataHandler,
+    which stores this data in a fixed GraphDataInterface, which we will use
+    for testing.
+    :param nx.DiGraph mock_graph: the mock graph on which we run the tests
+    :return GraphDataInterface: the fixed data interface used for testing
+    """
+    graph_data_handler = GraphDataHandler(
+        graph=mock_graph
+    )
+    graph_data_handler.run()
+
+    return graph_data_handler.graph_data_interface
+
+
+@pytest.mark.parametrize('expected_min_date', [
+    datetime(year=1999, month=12, day=20)
+])
+def test_min_date(graph_data_interface: GraphDataInterface,
+                  expected_min_date: datetime
+                  ):
+    """
+    Tests whether the minimum date is correctly calculated or not.
+    :param GraphDataInterface graph_data_interface: contains the results of the
+           minimum date calculation
+    :param datetime expected_min_date: the expected correct result for the
+           minimum date calculation
+    """
+    assert graph_data_interface.min_date == expected_min_date
+
+
+@pytest.mark.parametrize('expected_stations', [
+    [1.0, 2.0, 3.0, 5.0]
+])
+def test_stations(graph_data_interface: GraphDataInterface,
+                  expected_stations: list
+                  ):
+    """
+    Tests whether the list of stations is correctly acquired or not.
+    :param GraphDataInterface graph_data_interface: contains the acquired list
+           of stations
+    :param list expected_stations: the expected correct list of stations
+    """
+    assert graph_data_interface.stations == expected_stations
+
+
+@pytest.mark.parametrize('expected_pos', [
+    {
+        ('1.0', '2000-01-01'): (12, 0),
+        ('1.0', '2000-01-06'): (17, 0),
+        ('1.0', '2000-01-08'): (19, 0),
+        ('2.0', '1999-12-31'): (11, 1),
+        ('2.0', '2000-01-04'): (15, 1),
+        ('3.0', '2000-01-03'): (14, 2),
+        ('3.0', '1999-12-20'): (0, 2),
+        ('5.0', '1999-12-24'): (4, 3)
+    }
+])
+def test_pos(graph_data_interface: GraphDataInterface,
+             expected_pos: dict
+             ):
+    """
+    Tests whether the future positions on the grid are correctly calculated or
+    not.
+    :param GraphDataInterface graph_data_interface: contains the calculated
+           mapping of nodes to their eventual positions on the grid
+    :param dict expected_pos: the expected correct mapping of nodes to their
+           eventual positions on the grid
+    """
+    assert graph_data_interface.pos == expected_pos
