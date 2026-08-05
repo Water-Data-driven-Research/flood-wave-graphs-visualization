7d8b7c4f69fa97e5ec00e4bdbf1ba67c66efae40
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-05 21:25:20 +0200

    Implement run function

--- fwg_visualization/data/time_series_data_handler.py
+++ fwg_visualization/data/time_series_data_handler.py
@@ -21,3 +21,16 @@ class TimeSeriesDataHandler:
 
         self.time_series_data_interface = TimeSeriesDataInterface()
 
+    def run(self):
+        """
+        Run function, preprocesses the data to make it easier to plot using
+        plotly express, then stores it in a TimeSeriesDataInterface.
+        """
+        combined_data_frame = pd.concat(
+            objs=self.data,
+            names=['station_pair']
+        ).reset_index()
+
+        self.time_series_data_interface = TimeSeriesDataInterface(
+            statistics=combined_data_frame
+        )
