2f9396b6e01996ea1d406960d4b33dadff893643
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-05 21:24:58 +0200

    Implement constructor

--- fwg_visualization/data/time_series_data_handler.py
+++ fwg_visualization/data/time_series_data_handler.py
@@ -17,4 +17,7 @@ class TimeSeriesDataHandler:
                keys: station pairs, values: pandas DataFrames (data of the
                given station pairs)
         """
-        pass
+        self.data = data
+
+        self.time_series_data_interface = TimeSeriesDataInterface()
+
