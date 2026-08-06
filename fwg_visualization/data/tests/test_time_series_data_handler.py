5eaf077ea0053ff55ba8623e9bc8f6023dfd7d7a
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-06 17:41:54 +0200

    Rename mock_data to mock_yearly_data

--- fwg_visualization/data/tests/test_time_series_data_handler.py
+++ fwg_visualization/data/tests/test_time_series_data_handler.py
@@ -20,55 +20,56 @@ def mock_year_range() -> tuple:
 
 
 @pytest.fixture
-def mock_data() -> dict:
+def mock_yearly_data() -> dict:
     """
     Creates a mock data dictionary on which we can test the code base.
     :return dict: the mock data on which the tests will be run
     """
-    mock_data: dict = {}
+    mock_yearly_data: dict = {}
     mock_dates = pd.Index(
         data=['1999', '2000', '2001', '2002', '2003', '2004', '2005'],
         dtype='period[Y-DEC]',
         name='date'
     )
 
-    mock_data[(300, 270)] = pd.DataFrame({
+    mock_yearly_data[(300, 270)] = pd.DataFrame({
         'value': [14, 17, 6, 20, 22, 19, 3]
     }).set_index(mock_dates)
 
-    mock_data[(220, 150)] = pd.DataFrame({
+    mock_yearly_data[(220, 150)] = pd.DataFrame({
         'value': [1, None, 4, 31, 21, None, 11]
     }).set_index(mock_dates)
 
-    mock_data[(120, 110)] = pd.DataFrame({
+    mock_yearly_data[(120, 110)] = pd.DataFrame({
         'value': [9, 22, None, 21, 11, 4, 5]
     }).set_index(mock_dates)
 
-    mock_data[(105, 40)] = pd.DataFrame({
+    mock_yearly_data[(105, 40)] = pd.DataFrame({
         'value': [31, 29, 12, 21, 14, 11, 56]
     }).set_index(mock_dates)
 
-    mock_data[(15, 2)] = pd.DataFrame({
+    mock_yearly_data[(15, 2)] = pd.DataFrame({
         'value': [48, 26, 5, 13, 0, 43, 32]
     }).set_index(mock_dates)
 
-    return mock_data
+    return mock_yearly_data
 
 
 @pytest.fixture
-def time_series_data_interface(mock_year_range,
-                               mock_data) -> TimeSeriesDataInterface:
+def time_series_data_interface(mock_year_range: tuple,
+                               mock_yearly_data: dict
+                               ) -> TimeSeriesDataInterface:
     """
     Preprocesses the received data using a TimeSeriesDataHandler, which then
     stores this preprocessed data in a TimeSeriesDataInterface, which we will
     use for testing.
     :param tuple mock_year_range: the year range for which we filter the data
-    :param dict mock_data: the mock data on which we run the tests
+    :param dict mock_yearly_data: the mock data on which we run the tests
     :return TimeSeriesDataInterface: the fixed data interface used for testing
     """
     time_series_data_handler = TimeSeriesDataHandler(
         year_range=mock_year_range,
-        data=mock_data
+        data=mock_yearly_data
     )
     time_series_data_handler.run()
 
