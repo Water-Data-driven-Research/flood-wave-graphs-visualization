26bebbf783db74ef181d793b59acda6eefcba983
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-06 16:18:55 +0200

    Change data interface member variable from data frame to dictionary

--- fwg_visualization/data/interfaces/time_series_data_interface.py
+++ fwg_visualization/data/interfaces/time_series_data_interface.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
 
-import pandas as pd
-
 
 @dataclass
 class TimeSeriesDataInterface:
     """
     Class for storing data preprocessed in a TimeSeriesDataHandler class.
-    :param pd.DataFrame statistics: the statistics to be stored
+    :param dict statistics: the statistics to be stored
     """
-    statistics: pd.DataFrame = None
+    statistics: dict = None
