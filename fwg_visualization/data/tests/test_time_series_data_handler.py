57fc19aa147f452a252ef3c232822d50d15d1378
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-07 15:02:33 +0200

    Fix import order

--- fwg_visualization/data/tests/test_time_series_data_handler.py
+++ fwg_visualization/data/tests/test_time_series_data_handler.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import pytest
 
-from fwg_visualization.data.time_series_data_handler import (
-    TimeSeriesDataHandler
-)
 from fwg_visualization.data.interfaces.time_series_data_interface import (
     TimeSeriesDataInterface
+)
+from fwg_visualization.data.time_series_data_handler import (
+    TimeSeriesDataHandler
 )
 
 
