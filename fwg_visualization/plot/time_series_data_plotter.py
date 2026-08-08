09d2e71a2c672c95b79f6ed7d9b00e4342a531f7
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-08 17:19:51 +0200

    Create methods

--- fwg_visualization/plot/time_series_data_plotter.py
+++ fwg_visualization/plot/time_series_data_plotter.py
@@ -15,3 +15,30 @@ class TimeSeriesDataPlotter:
         :param TimeSeriesDataInterface ts_data_if: contains data to be plotted
         """
         self.statistics = ts_data_if.statistics
+
+    def plot_ts_data(self,
+                     statistic: str,
+                     graph_name: str,
+                     unit_of_measure: str,
+                     rkm_station: dict):
+        """
+        Creates the plot of the received data.
+        :param str statistic: the statistic that we are plotting, one of the
+               following: 'flood wave count', 'mean propagation time',
+               'median propagation time', 'mode propagation time'
+        :param str graph_name: what the name of the graph should be
+        :param str unit_of_measure: the unit of measurement used on the y-axis
+        :param dict rkm_station: the dictionary that maps station positions on
+               the river to their names
+        """
+        pass
+
+    @staticmethod
+    def create_layout(fig: go.Figure, graph_name: str, unit_of_measure: str):
+        """
+        Creates the layout of the received figure.
+        :param go.Figure fig: the figure to customize
+        :param str graph_name: what the name of the graph should be
+        :param str unit_of_measure: the unit of measurement used on the y-axis
+        """
+        pass
