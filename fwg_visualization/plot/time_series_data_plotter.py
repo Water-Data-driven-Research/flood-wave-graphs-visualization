a83d5de320c983eb4800eef7626af75d848475f9
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-08 19:13:33 +0200

    Fix add_trace

--- fwg_visualization/plot/time_series_data_plotter.py
+++ fwg_visualization/plot/time_series_data_plotter.py
@@ -35,8 +35,8 @@ class TimeSeriesDataPlotter:
         fig = go.Figure()
         for pair, df in self.statistics.items():
             fig.add_trace(trace=go.Scatter(
-                x=df.index,
-                y=df[statistic],
+                x=df.index.strftime('%Y').tolist(),
+                y=df[statistic].strftime('%Y').tolist(),
                 mode='lines',
                 name=f'{rkm_station[pair[0]]}---{rkm_station[pair[1]]}'
             ))
