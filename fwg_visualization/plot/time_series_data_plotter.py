5bcd74a7925d579e372c79eb472e837689fea697
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-10 12:28:10 +0200

    Turn statistic (renamed to statistic_type to enhance readability) and rkm_station into member variables (because these are uniquely determined by the data interface)

--- fwg_visualization/plot/time_series_data_plotter.py
+++ fwg_visualization/plot/time_series_data_plotter.py
@@ -9,42 +9,44 @@ class TimeSeriesDataPlotter:
     """
     This class creates the plot of the received time series data.
     """
-    def __init__(self, data_if: TimeSeriesDataInterface):
+    def __init__(self,
+                 data_if: TimeSeriesDataInterface,
+                 statistic_type: str,
+                 rkm_station: str):
         """
         Constructor.
         :param TimeSeriesDataInterface data_if: contains data to be plotted
-        """
-        self.statistics = data_if.statistics
-
-    def plot_ts_data(self,
-                     statistic: str,
-                     rkm_station: dict,
-                     width: int = 1000,
-                     height: int = 500):
-        """
-        Creates the plot of the received data.
-        :param str statistic: the statistic that we are plotting, one of
+        :param str statistic_type: the statistic that we are plotting, one of
                the following: 'flood wave count', 'mean propagation time',
                'median propagation time' (uniquely determined by what the data
                interface includes)
         :param dict rkm_station: the dictionary that maps station positions on
                the river to their names
+        """
+        self.statistics = data_if.statistics
+        self.statistic_type = statistic_type
+        self.rkm_station = rkm_station
+
+    def plot_ts_data(self,
+                     width: int = 1000,
+                     height: int = 500):
+        """
+        Creates the plot of the received data.
         :param int width: the width of the image to be created (pixels)
         :param int height: the height of the image to be created (pixels)
         """
-        unit_of_measurement = self.determine_unit_of_measurement(
-            statistic=statistic
-        )
-        graph_name = self.determine_graph_name(statistic=statistic)
+        unit_of_measurement = self.determine_unit_of_measurement()
+        graph_name = self.determine_graph_name()
 
         fig = go.Figure()
 
         for pair, df in self.statistics.items():
             fig.add_trace(trace=go.Scatter(
                 x=df.index.astype(str).tolist(),
-                y=df[statistic],
+                y=df[self.statistic_type],
                 mode='lines',
-                name=f'{rkm_station[pair[0]]}---{rkm_station[pair[1]]}'
+                name=f'{self.rkm_station[pair[0]]}---'
+                     f'{self.rkm_station[pair[1]]}'
             ))
 
         self.create_layout(fig=fig,
@@ -89,48 +91,39 @@ class TimeSeriesDataPlotter:
             hovermode='x unified'
         )
 
-    @staticmethod
-    def determine_unit_of_measurement(statistic: str) -> str:
+    def determine_unit_of_measurement(self) -> str:
         """
         Finds the unit of measurement that we will be using from the type of
         statistics that we are plotting.
-        :param str statistic: the statistic that we are plotting, one of
-               the following: 'flood wave count', 'mean propagation time',
-               'median propagation time' (uniquely determined by what the data
-               interface includes)
         :raise ValueError: if the statistic type given is unsupported
         :return str: the unit of measurement that we will display on the y-axis
                 of the graph
         """
-        if statistic == 'flood wave count':
+        if self.statistic_type == 'flood wave count':
             unit_of_measurement = 'Number'
-        elif 'propagation time' in statistic:
+        elif 'propagation time' in self.statistic_type:
             unit_of_measurement = 'Days'
         else:
             raise ValueError(
-                f"Unsupported statitic type: {statistic}, use one "
+                f"Unsupported statitic type: {self.statistic_type}, use one "
                 "of the following: 'flood wave count', "
                 "'mean propagation time', 'median propagation time'."
             )
         return unit_of_measurement
 
-    def determine_graph_name(self, statistic: str) -> str:
+    def determine_graph_name(self) -> str:
         """
         Determines the name of the graph based on the type of statistics that
         we are plotting and on the frequency of our aggregated data.
-        :param str statistic: the statistic that we are plotting, one of
-               the following: 'flood wave count', 'mean propagation time',
-               'median propagation time' (uniquely determined by what the data
-               interface includes)
         :raise ValueError: if the period frequency of the pandas DataFrames
                is unsupported
         :return str: the name of the graph
         """
         df_test = list(self.statistics.values())[0]
         if df_test.index.freq == 'YE':
-            graph_name = f'Yearly {statistic}'
+            graph_name = f'Yearly {self.statistic_type}'
         elif df_test.index.freq == 'QE':
-            graph_name = f'Quarterly {statistic}'
+            graph_name = f'Quarterly {self.statistic_type}'
         else:
             raise ValueError(
                 f'Unsupported period frequency: {df_test.index.freq}.'
