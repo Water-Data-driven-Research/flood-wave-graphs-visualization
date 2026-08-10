de660f73ca7afcbb1861791e1813ccff4d45daa0
balazsmindszenti <mindszenti.balazs@gmail.com>
2026-08-10 14:42:32 +0200

    dev/implement-time-series-data-plotting (#16)
    * Shorten parameter name
    
    * Implement constructor
    
    * Update imports
    
    * Create methods
    
    * Implement plot_ts_data method
    
    * Implement create_layout static method
    
    * Rename constructor parameter
    
    * Rename parameters
    
    * Fix line breaks
    
    * Fix add_trace
    
    * Fix add_trace y argument
    
    * Add width and height parameters
    
    * Create support for quarterly data visualization
    
    * Replace manual graph naming with a standardized convention
    
    * Fix quarterly graphing
    
    * Maximize number of ticks on the x axis
    
    * Remove manual unit measurement assignment
    
    * Reduce maximum ticks on the x axis to 40
    
    * Fix value error message
    
    * Increase maximum ticks on the x axis to 50
    
    * Add dots to end of error messages
    
    * Change docstring
    
    * Fix dot at the end of error message
    
    * Fix error message
    
    * Increase documentation precision
    
    * Create determine_unit_of_measurement static method
    
    * Create determine_graph_name method
    
    * Turn statistic (renamed to statistic_type to enhance readability) and rkm_station into member variables (because these are uniquely determined by the data interface)
    
    * Reorder methods
    
    * Fix typo
    
    * Change statistic_type from manually added variable to calculated variable
    
    * Change parameter formatting
    
    * Update constructor formatting
    
    * Rename and change role of plotting method
    
    * Fix docstring

--- fwg_visualization/plot/time_series_data_plotter.py
+++ fwg_visualization/plot/time_series_data_plotter.py
@@ -1,4 +1,4 @@
-import plotly
+import plotly.graph_objects as go
 
 from fwg_visualization.data.interfaces.time_series_data_interface import (
     TimeSeriesDataInterface
@@ -9,10 +9,129 @@ class TimeSeriesDataPlotter:
     """
     This class creates the plot of the received time series data.
     """
-    def __init__(self, time_series_data_interface: TimeSeriesDataInterface):
+    def __init__(self, data_if: TimeSeriesDataInterface, rkm_station: str):
         """
         Constructor.
-        :param TimeSeriesDataInterface time_series_data_interface: contains
-               data to be plotted
+        :param TimeSeriesDataInterface data_if: contains data to be plotted
+        :param dict rkm_station: the dictionary that maps station positions on
+               the river to their names
         """
-        pass
+        self.statistics = data_if.statistics
+        self.rkm_station = rkm_station
+
+        self.__statistic_type = ''
+
+    @property
+    def statistic_type(self) -> str:
+        """
+        Acquires the type of statistic that we are plotting, one of the
+        following: 'flood wave count', 'mean propagation time',
+        'median propagation time'.
+        :return str: the type of statistics that we are plotting
+        """
+        self.__statistic_type = list(self.statistics.values())[0].columns[0]
+        return self.__statistic_type
+
+    def get_ts_data_plot(self,
+                         width: int = 1000,
+                         height: int = 500) -> go.Figure:
+        """
+        Creates the plot of the received data.
+        :param int width: the width of the image to be created (pixels)
+        :param int height: the height of the image to be created (pixels)
+        :return go.Figure: the created plot
+        """
+        unit_of_measurement = self.determine_unit_of_measurement()
+        graph_name = self.determine_graph_name()
+
+        fig = go.Figure()
+
+        for pair, df in self.statistics.items():
+            fig.add_trace(trace=go.Scatter(
+                x=df.index.astype(str).tolist(),
+                y=df[self.statistic_type],
+                mode='lines',
+                name=f'{self.rkm_station[pair[0]]}---'
+                     f'{self.rkm_station[pair[1]]}'
+            ))
+
+        self.create_layout(fig=fig,
+                           graph_name=graph_name,
+                           unit_of_measurement=unit_of_measurement,
+                           width=width,
+                           height=height)
+        return fig
+
+    def determine_unit_of_measurement(self) -> str:
+        """
+        Finds the unit of measurement that we will be using from the type of
+        statistics that we are plotting.
+        :raise ValueError: if the statistic type given is unsupported
+        :return str: the unit of measurement that we will display on the y-axis
+                of the graph
+        """
+        if self.statistic_type == 'flood wave count':
+            unit_of_measurement = 'Number'
+        elif 'propagation time' in self.statistic_type:
+            unit_of_measurement = 'Days'
+        else:
+            raise ValueError(
+                f"Unsupported statistic type: {self.statistic_type}, use one "
+                "of the following: 'flood wave count', "
+                "'mean propagation time', 'median propagation time'."
+            )
+        return unit_of_measurement
+
+    def determine_graph_name(self) -> str:
+        """
+        Determines the name of the graph based on the type of statistics that
+        we are plotting and on the frequency of our aggregated data.
+        :raise ValueError: if the period frequency of the pandas DataFrames
+               is unsupported
+        :return str: the name of the graph
+        """
+        df_test = list(self.statistics.values())[0]
+        if df_test.index.freq == 'YE':
+            graph_name = f'Yearly {self.statistic_type}'
+        elif df_test.index.freq == 'QE':
+            graph_name = f'Quarterly {self.statistic_type}'
+        else:
+            raise ValueError(
+                f'Unsupported period frequency: {df_test.index.freq}.'
+            )
+        return graph_name
+
+    @staticmethod
+    def create_layout(fig: go.Figure,
+                      graph_name: str,
+                      unit_of_measurement: str,
+                      width: int,
+                      height: int):
+        """
+        Creates the layout of the received figure.
+        :param go.Figure fig: the figure to customize
+        :param str graph_name: what the name of the graph should be
+        :param str unit_of_measurement: the unit of measurement used on the
+               y-axis
+        :param int width: the width of the image to be created (pixels)
+        :param int height: the height of the image to be created (pixels)
+        """
+        fig.update_layout(
+            title={
+                'text': graph_name,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'x': 0.5,
+                'y': 0.98
+            },
+            width=width,
+            height=height,
+            margin=dict(l=20, r=20, t=30, b=20),
+            xaxis={
+                'title': dict(text='Date'),
+                'nticks': 50
+            },
+            yaxis=dict(title=dict(text=unit_of_measurement)),
+            legend=dict(title=dict(text='Station pairs')),
+            hovermode='x unified'
+        )
