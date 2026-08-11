62048fb2aa5621c7752953bf5f28d6d095bde417
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-11 17:16:37 +0200

    Improve edge data creation documentation

--- fwg_visualization/data/graph_data_handler.py
+++ fwg_visualization/data/graph_data_handler.py
@@ -143,7 +143,7 @@ class GraphDataHandler:
     def create_edge_data(self) -> dict:
         """
         Creates the arrows that are the edges of the directed graph, as well
-        as markers to create the hover function.
+        as markers to create the hover functionality.
         :return dict: contains the data, its keys are: 'arrows' (the value is
                 the list containing every directed edge, each stored as a
                 dictionary), and 'markers' (the value is another dictionary,
