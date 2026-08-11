cc871cb078ee28ff54b3a7765b84c129e644133c
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-11 15:06:15 +0200

    Create empty methods

--- fwg_visualization/plot/graph_plotter.py
+++ fwg_visualization/plot/graph_plotter.py
@@ -16,3 +16,48 @@ class GraphPlotter:
                plotting
         """
         pass
+
+    def get_graph_plot(self,
+                       graph_name: str = 'Flood Wave Graph',
+                       width: int = 1000,
+                       height: int = 500) -> go.Figure:
+        """
+        Creates the plot of the graph.
+        :param str graph_name: 'Flood Wave Graph' or 'Flood Map'
+        :param int width: the width of the image to be created (pixels)
+        :param int height: the height of the image to be created (pixels)
+        :return go.Figure:
+        """
+        pass
+
+    def create_node_trace(self, node_data: dict) -> go.Scatter:
+        """
+        Creates the trace that includes the nodes of the graph.
+        :return go.Scatter: the trace of the nodes
+        """
+        pass
+
+    def create_edge_hover_trace(self) -> go.Scatter:
+        """
+        Creates the trace that includes the invisible markers that simulate
+        the hover function for the edges.
+        :return go.Scatter: the miniscule markers in the middle of the edges
+        """
+        pass
+
+    @staticmethod
+    def create_layout(fig: go.Figure,
+                      axis_data: dict,
+                      graph_name: str,
+                      width: int,
+                      height: int):
+        """
+        Creates the layout of the received figure.
+        :param go.Figure fig: updates the layout of this figure
+        :param dict axis_data: the data to display on the axes of the figure
+        :param str graph_name: the name of the graph ('Flood Wave Graph' or
+               'Flood Map')
+        :param int width: the width of the image to be created (pixels)
+        :param int height: the height of the image to be created (pixels)
+        """
+        pass
