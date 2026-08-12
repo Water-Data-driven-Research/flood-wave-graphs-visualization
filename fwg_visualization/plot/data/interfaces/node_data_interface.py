7effe9ee32bf51ca81ff36593c7474ba4e72120c
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-12 16:16:14 +0200

    Update member variable names of NodeDataInterface

--- fwg_visualization/plot/data/interfaces/node_data_interface.py
+++ fwg_visualization/plot/data/interfaces/node_data_interface.py
@@ -5,10 +5,10 @@ from dataclasses import  dataclass
 class NodeDataInterface:
     """
     Class for storing data calculated in a NodeDataCalculator class.
-    :param list x_coords: the x-coordinates of the nodes
-    :param list y_coords: the y-coordinates of the nodes
+    :param list x_coordinates: the x-coordinates of the nodes
+    :param list y_coordinates: the y-coordinates of the nodes
     :param list text_data: data required to make the hover texts of the nodes
     """
-    x_coords: list = None
-    y_coords: list = None
+    x_coordinates: list = None
+    y_coordinates: list = None
     text_data: list = None
