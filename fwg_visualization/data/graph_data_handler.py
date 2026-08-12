e789f0994a2ffbaccc2b7308bb0359dde04670fb
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-12 18:33:34 +0200

    Move edge data calculation

--- fwg_visualization/data/graph_data_handler.py
+++ fwg_visualization/data/graph_data_handler.py
@@ -11,23 +11,13 @@ class GraphDataHandler:
     """
     This class preprocesses a flood wave graph or flood map for visualization.
     """
-    def __init__(self,
-                 graph: nx.DiGraph,
-                 rkm_station: dict,
-                 level_group: dict):
+    def __init__(self, graph: nx.DiGraph):
         """
         Constructor.
         :param nx.DiGraph graph: the fwg or flood map to be preprocessed
-        :param dict rkm_station: the dictionary that maps station positions on
-               the river to their names
-        :param dict level_group: the dictionary that maps station positions on
-               the river to their level groups (values above which a water
-               level is considered high)
         """
         self.graph_nodes = list(graph.nodes())
         self.graph_edges = list(graph.edges())
-        self.rkm_station = rkm_station
-        self.level_group = level_group
 
         self.graph_data_interface = GraphDataInterface()
         self.min_date: datetime = datetime.min
@@ -41,8 +31,6 @@ class GraphDataHandler:
         """
         self.get_min_date()
         self.get_stations()
-
-        edge_data = self.create_edge_data()
 
         self.graph_data_interface = GraphDataInterface(
             graph_nodes=self.graph_nodes,
@@ -69,58 +57,3 @@ class GraphDataHandler:
         self.stations = sorted(list(set(
             [float(node[0]) for node in self.graph_nodes]
         )))
-
-    def create_edge_data(self) -> dict:
-        """
-        Creates the data required to make the arrows that are the edges of the
-        directed graph, as well as data required to make very small markers
-        (edge hover trace) to implement the hover functionality on edges.
-        :return dict: contains the data, its keys are: 'directed_edge_data',
-                'x_coords', 'y_coords' and 'text'
-        """
-        directed_edge_data = []
-        x_coords = []
-        y_coords = []
-        text = []
-
-        for start, end in self.graph_edges:
-            x_start = self.pos[start][0]
-            x_end = self.pos[end][0]
-            y_start = self.pos[start][1]
-            y_end = self.pos[end][1]
-
-            x_coords.append((x_start + x_end) / 2)
-            y_coords.append((y_start + y_end) / 2)
-
-            dx = x_end - x_start
-            dy = y_end - y_start
-
-            edge_data_dict = {
-                'x_start': x_start + dx / 80,
-                'y_start': y_start + dy / 80,
-                'x_end': x_end - dx / 15,
-                'y_end': y_end - dy / 15
-            }
-
-            directed_edge_data.append(edge_data_dict)
-
-            start_name = self.rkm_station[float(start[0])]
-            start_km = start[0]
-            start_date = start[1]
-
-            end_name = self.rkm_station[float(end[0])]
-            end_km = end[0]
-            end_date = end[1]
-
-            text.append(
-                (start_name, start_km, start_date, end_name, end_km, end_date)
-            )
-
-        edge_data = {
-            'directed_edge_data': directed_edge_data,
-            'x_coords': x_coords,
-            'y_coords': y_coords,
-            'text': text
-        }
-
-        return edge_data
--- fwg_visualization/plot/data/edge_data_calculator.py
+++ fwg_visualization/plot/data/edge_data_calculator.py
@@ -17,26 +17,88 @@ class EdgeDataCalculator:
     """
     def __init__(self,
                  graph_data_if: GraphDataInterface,
-                 node_data_if: NodeDataInterface):
+                 node_data_if: NodeDataInterface,
+                 rkm_station: dict):
         """
         Constructor.
         :param GraphDataInterface graph_data_if: contains data about the graph
         :param NodeDataInterface node_data_if: contains data about the nodes
+        :param dict rkm_station: the dictionary that maps station positions on
+               the river to their names
         """
-        pass
+        self.graph_edges = graph_data_if.graph_edges
+        self.positions = node_data_if.positions
+        self.rkm_station = rkm_station
+
+        self.edge_data_if = EdgeDataInterface()
 
     def run(self):
         """
         Run function, calculates the required edge data and instantiates an
         EdgeDataInterface to store it.
         """
-        pass
+        edge_data = self.get_edge_data()
 
-    def get_edge_data(self):
+        self.edge_data_if = EdgeDataInterface(
+            directed_edge_data=edge_data['directed_edge_data'],
+            x_coordinates=edge_data['directed_edge_data'],
+            y_coordinates=edge_data['directed_edge_data'],
+            text_data=edge_data['directed_edge_data']
+        )
+
+    def get_edge_data(self) -> dict:
         """
         Calculates the positions of the directed edges and the positions of
         the small markers used to create hover functionality for the edges,
         and the data required to make the hover text of the edges.
         :return dict: the data we need about the edges
         """
-        pass
+        directed_edge_data: list = []
+        x_coordinates: list = []
+        y_coordinates: list = []
+        text_data: list = []
+
+        for start, end in self.graph_edges:
+            start_node_pos = self.positions[start]
+            end_node_pos = self.positions[end]
+
+            x_start = start_node_pos[0]
+            x_end = end_node_pos[0]
+            y_start = start_node_pos[1]
+            y_end = end_node_pos[1]
+
+            x_coordinates.append((x_start + x_end) / 2)
+            y_coordinates.append((y_start + y_end) / 2)
+
+            dx = x_end - x_start
+            dy = y_end - y_start
+
+            edge_data_dict = {
+                'x_start': x_start + dx / 80,
+                'y_start': y_start + dy / 80,
+                'x_end': x_end - dx / 15,
+                'y_end': y_end - dy / 15
+            }
+
+            directed_edge_data.append(edge_data_dict)
+
+            start_name = self.rkm_station[float(start[0])]
+            start_km = start[0]
+            start_date = start[1]
+
+            end_name = self.rkm_station[float(end[0])]
+            end_km = end[0]
+            end_date = end[1]
+
+            text_data.append(
+                (start_name, start_km, start_date, end_name, end_km, end_date)
+            )
+
+        edge_data = {
+            'directed_edge_data': directed_edge_data,
+            'x_coordinates': x_coordinates,
+            'y_coordinates': y_coordinates,
+            'text_data': text_data
+        }
+
+        return edge_data
