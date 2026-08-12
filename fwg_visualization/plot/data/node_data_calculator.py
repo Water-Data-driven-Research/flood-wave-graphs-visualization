fe2e9d11054b6c67d52dd7b0cf4901452c282ff6
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-12 16:25:01 +0200

    Update node data calculator member variables and documentation

--- fwg_visualization/plot/data/node_data_calculator.py
+++ fwg_visualization/plot/data/node_data_calculator.py
@@ -13,11 +13,16 @@ class NodeDataCalculator:
     Calculates the data required to make the node markers (node trace).
     """
     def __init__(self,
-                 graph_if: GraphDataInterface,
+                 graph_data_if: GraphDataInterface,
                  rkm_station: dict,
                  level_group: dict):
         """
         Constructor.
-        :param GraphDataInterfcae
+        :param GraphDataInterface graph_data_if: contains data about the graph
+        :param dict rkm_station: the dictionary that maps station positions on
+               the river to their names
+        :param dict level_group: the dictionary that maps station positions on
+               the river to their level groups (values above which a water
+               level is considered high)
         """
         pass
