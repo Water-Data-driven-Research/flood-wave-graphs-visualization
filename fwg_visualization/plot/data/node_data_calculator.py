288aa79bec10f4b7c67eb647ad07366d9d7fd1e2
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-16 14:31:02 +0200

    Add level differences to node data

--- fwg_visualization/plot/data/node_data_calculator.py
+++ fwg_visualization/plot/data/node_data_calculator.py
@@ -13,12 +13,21 @@ class NodeDataCalculator:
     def __init__(self,
                  graph_data_if: GraphDataInterface,
                  rkm_station: dict,
+                 vertex_data: dict,
                  level_group: dict):
         """
         Constructor.
         :param GraphDataInterface graph_data_if: contains data about the graph
         :param dict rkm_station: the dictionary that maps station positions on
                the river to their names
+        :param dict vertex_data: data about the delta peaks, structure:
+               {station1: {date1: {'value': value1, 'color': color1},
+                           date2: {'value': value2, 'color': color2},
+                           ...},
+                station2: {date1: {'value': value1, 'color': color1},
+                           date2: {'value': value2, 'color': color2},
+                           ...},
+                ...}
         :param dict level_group: the dictionary that maps station positions on
                the river to their level groups (values above which a water
                level is considered high)
@@ -26,6 +35,7 @@ class NodeDataCalculator:
         self.graph_nodes = graph_data_if.graph_nodes
         self.positions = graph_data_if.positions
         self.rkm_station = rkm_station
+        self.vertex_data = vertex_data
         self.level_group = level_group
 
         self.node_data_if = NodeDataInterface()
@@ -39,6 +49,7 @@ class NodeDataCalculator:
 
         self.node_data_if.x_coordinates = node_data['x_coordinates']
         self.node_data_if.y_coordinates = node_data['y_coordinates']
+        self.node_data_if.level_differences = node_data['level_differences']
         self.node_data_if.text_data = node_data['text_data']
 
     def get_node_data(self) -> dict:
@@ -51,21 +62,26 @@ class NodeDataCalculator:
             (coord[0], coord[1]) for coord in self.positions.values()
         ])
 
+        level_differences: list = []
         text_data: list = []
 
         for node in self.graph_nodes:
             node_date_str = node[1]
             station_name = self.rkm_station[float(node[0])]
             station_km = node[0]
+            water_level = self.vertex_data[station_km][node_date_str]['value']
             station_level_group = self.level_group[node[0]]
+            level_differences.append(water_level - station_level_group)
 
             text_data.append(
-                (node_date_str, station_name, station_km, station_level_group)
+                (node_date_str, station_name, station_km, water_level,
+                 station_level_group)
             )
 
         node_data = {
             'x_coordinates': x_coordinates,
             'y_coordinates': y_coordinates,
+            'level_differences': level_differences,
             'text_data': text_data
         }
 
