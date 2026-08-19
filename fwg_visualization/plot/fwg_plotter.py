e46ebcf747c8a8279a1e0ee21b7b4d410ea843e3
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-19 22:35:20 +0200

    Add x-axis range setting

--- fwg_visualization/plot/fwg_plotter.py
+++ fwg_visualization/plot/fwg_plotter.py
@@ -131,6 +131,14 @@ class FWGPlotter:
         :param int width: the width of the image to be created (pixels)
         :param int height: the height of the image to be created (pixels)
         """
+        x_ticks = self.axis_data_if.x_ticks
+        x_tick_labels = self.axis_data_if.x_tick_labels
+
+        x_length = max(x_ticks) - min(x_ticks)
+        x_range_end = min(4.0, 0.1 * x_length)
+        x_range = [min(x_ticks) - x_range_end,
+                   max(x_ticks) + x_range_end]
+
         y_ticks = self.axis_data_if.y_ticks
         y_tick_labels = self.axis_data_if.y_tick_labels
 
@@ -151,8 +159,9 @@ class FWGPlotter:
             margin=dict(b=20, l=20, r=20, t=30),
             xaxis={
                 'title': dict(text='Date'),
-                'tickvals': self.axis_data_if.x_ticks,
-                'ticktext': self.axis_data_if.x_tick_labels
+                'tickvals': x_ticks,
+                'ticktext': x_tick_labels,
+                'range': x_range
             },
             yaxis={
                 'title': dict(text='River kilometer'),
