ae34d3a4d774ae9aabf82b0b2e2d91f70618fca3
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-19 19:34:05 +0200

    Implement FloodMapPlotter wrapper class

---
+++ fwg_visualization/plot/flood_map_plotter.py
@@ -0,0 +1,39 @@
+from typing import Type
+
+import plotly.graph_objects as go
+
+from fwg_visualization.plot.fwg_plotter import FWGPlotter
+
+
+def flood_map_plotter(cls: Type[FWGPlotter]) -> Type[FWGPlotter]:
+    """
+    Acts as a decorator to add flood map plotting functionality.
+    :param Type[FWGPlotter] cls: the FWG plotter to which we add a new method
+    :return:
+    """
+    class FloodMapPlotter(cls):
+        """
+        This class adds flood map plotting functionality to an FWGPlotter.
+        """
+        def get_flood_map_plot(self,
+                               graph_name: str = 'Flood Map',
+                               width: int = 1000,
+                               height: int = 500) -> go.Figure:
+            """
+            Creates the plot of the flood map.
+            :param str graph_name: 'Flood Map' by default
+            :param int width: the width of the image to be created (pixels)
+            :param int height: the height of the image to be created (pixels)
+            :return go.Figure: the created plot
+            """
+            fig = self.get_fwg_plot(graph_name=graph_name,
+                                    width=width,
+                                    height=height)
+
+            boundary_stations = list(set(self.node_data_if.y_coordinates))
+            for station in boundary_stations:
+                fig.add_hline(y=station, line_color='red')
+
+            return fig
+
+    return FloodMapPlotter
