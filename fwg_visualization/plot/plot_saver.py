647526314a5f2273d2243da164a1ee27e2f06402
Balázs Mindszenti <mindszenti.balazs@gmail.com>
2026-08-31 20:17:19 +0200

    dev/implement-plot-saver (#36)
    * Rename GraphSaver to PlotSaver
    
    * Implement PlotSaver
    
    * Rename py file
    
    * Update documentation
    
    * Remove line break

---
+++ fwg_visualization/plot/plot_saver.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import plotly.graph_objects as go
+
+
+class PlotSaver:
+    """
+    This static class saves a created plot as a static image (by default PDF).
+    """
+    @staticmethod
+    def save_plot(fig: go.Figure,
+                  folder_path: Path,
+                  file_name: str = 'plot.pdf'):
+        """
+        Saves a created plot (as a PDF file by default).
+        :param go.Figure fig: the plot to be saved
+        :param Path folder_path: where the file will be saved
+        :param str file_name: the name of the saved file
+        """
+        folder_path.mkdir(parents=True, exist_ok=True)
+        fig.write_image(folder_path / file_name)
